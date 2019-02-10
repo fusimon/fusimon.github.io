c9e767e04d375050610526a8a361dbcfb0734fd6
fusimon <626303495@qq.com>
2019-02-10 21:24:47 +0800

    first commit

---
+++ js/pulic.js
@@ -0,0 +1,7 @@
+
+//$("#home_img").height(h).width(w);
+$(window).resize(function(){
+          var h = $(window).height();
+          var w = $(window).width();
+          console.log("窗口高度：" + h + "; 窗口宽度：" + w);
+    	});
